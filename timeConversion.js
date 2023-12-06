6bd2f21a9a00ce112bc5a8853b9103ce80e04439
praneethandukuri <praneethandukuri@gmail.com>
2023-12-06 18:57:07 +0530

    create a function that returns 12-hours format to 24-hours format

---
+++ timeConversion.js
@@ -0,0 +1,17 @@
+
+const convertHours = function (hours, isAM) {
+  const numberOfHours = +hours
+  if (numberOfHours < 12 && !isAM) {
+    return numberOfHours + 12;
+  } else if (numberOfHours === 12 && isAM) {
+    return "00"
+  }
+  return hours;
+}
+const GETTIMECONVERTION = function (timeFormat) {
+  const isAM = timeFormat.toUpperCase().includes('AM');
+  const formatConversion = timeFormat.slice(0, -2);
+  const [hours, minutes, seconds] = formatConversion.split(':');
+  return `${convertHours(hours, isAM)}:${minutes}:${seconds}`;
+}
+console.log(GETTIMECONVERTION('12:00:01am'));
